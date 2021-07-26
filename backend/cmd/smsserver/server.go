7dcd69705f13461441ee32ff8eb8d577a6fbe94a
Vinay Emani <vinnymaker18@gmail.com>
2021-07-26 11:31:10 +0530

    SMS-ON-PC Basic backend api implementation

--- backend/cmd/smsserver/server.go
+++ backend/cmd/smsserver/server.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/vinnymaker18/sms-on-pc/backend/common"
+	"github.com/vinnymaker18/sms-on-pc/backend/storage"
 )
 
 const (
@@ -15,20 +21,74 @@ const (
 	controlPort = 8001
 )
 
+func parseTextMessage(req *http.Request) (*common.SMSMessage, error) {
+	form := req.Form
+
+	// each message post request has user_id, from_addr, body fields in it.
+	if _, ok := form["userid"]; !ok {
+		return nil, fmt.Errorf("no user_id field found in request")
+	}
+
+	userID, err := strconv.ParseInt(form["userid"][0], 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid user_id in request")
+	}
+
+	originAddress, ok := form["origin"]
+	if !ok {
+		return nil, fmt.Errorf("no origin address field in request")
+	}
+
+	msgBody, ok := form["msgbody"]
+	if !ok {
+		return nil, fmt.Errorf("no message body in request")
+	}
+
+	return &common.SMSMessage{
+		UserID:        int64(userID),
+		Time:          time.Now(),
+		MsgBody:       msgBody[0],
+		OriginAddress: originAddress[0],
+	}, nil
+}
+
+func smsReadHandler(w http.ResponseWriter, req *http.Request) {
+	if _, ok := req.Form["userid"]; !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	userID, err := strconv.ParseInt(req.Form["userid"][0], 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// TODO - Ignore any serialization errors for now, come back to this and fix it
+	serializedBytes, _ := json.Marshal(storage.FetchNewSMS(userID))
+	w.Write(serializedBytes)
+}
+
+func smsWriteHandler(w http.ResponseWriter, req *http.Request) {
+	newTextMsg, err := parseTextMessage(req)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	storage.StoreNewSMS(newTextMsg)
+}
+
 func main() {
 	http.HandleFunc("/sms", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set(contentTypeHeader, "text/plain")
+		w.Header().Set(contentTypeHeader, jsonContentType)
 		req.ParseForm()
 
 		if req.Method == http.MethodGet {
-
-			w.Write([]byte("As"))
+			smsReadHandler(w, req)
 		} else if req.Method == http.MethodPost {
-			decoder := json.NewDecoder(req.Body)
-			reqParams := make(map[string]string)
-			decoder.Decode(&reqParams)
-
-			w.Write([]byte("okay!"))
+			smsWriteHandler(w, req)
 		}
 	})
 
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/vinnymaker18/sms-on-pc/backend
 
 go 1.15
+
+require github.com/jackc/pgx/v4 v4.13.0
